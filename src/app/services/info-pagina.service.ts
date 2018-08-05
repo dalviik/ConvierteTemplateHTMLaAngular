54e9b904a16dec85fb3ba285e4ce362e5bc46d89
dalviik <duglasapazaa@gmail.com>
2018-08-04 21:05:02 -0400

    Consumiendo desde una url en Firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,31 +1,48 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-pagina.interface';
+  import { Injectable } from '@angular/core';
+  import { HttpClient } from '@angular/common/http';
+  import { InfoPagina } from '../interfaces/info-pagina.interface';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class InfoPaginaService {
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class InfoPaginaService {
 
-  info: InfoPagina = {};
-  cargada = false;
+    info: InfoPagina = {};
+    cargada = false;
+    equipo: any[] = [];
 
-  constructor( private http: HttpClient ) {
+    constructor( private http: HttpClient ) {
 
-     console.log('Servicio de Info Pagina');
+      // this.cargarInfo();
+      this.cargarEquipo();
 
-    //  Leer el archivo Json
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
-      this.cargada = true;
-      this. info = resp;
-      console.log(resp);
-      // console.log(resp['email']);
+    }
 
+    private  cargarInfo() {
+      //  Leer el archivo Json
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this. info = resp;
+        // console.log(resp);
+        // console.log(resp['email']);
 
       });
 
     }
 
+    private cargarEquipo() {
+      //  Cargar desde url Json
+      this.http.get('https://angular-html-86dfb.firebaseio.com/equipo.json')
+      .subscribe( (resp: any ) => {
+        this. equipo = resp;
+        console.log(resp);
+        // console.log(resp['email']);
 
-}
+      });
+    }
+
+  }
+
+
+
